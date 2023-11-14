b8b52a8c7485ee7a83c214c97d9e37c85dd3bfb9
jacobhokanson <jacob.hokanson9@gmail.com>
2023-11-14 15:55:43 -0800

    insert and revalidate

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -1,6 +1,8 @@
 'use server';
 
 import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -19,4 +21,12 @@ export async function createInvoice(formData: FormData) {
     status: formData.get('status'),
   });
   const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath('/dashboard/invoices');
 }
