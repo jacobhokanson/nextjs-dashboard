cebdcfd02827712c93743126f953c2755d77b227
jacobhokanson <jacob.hokanson9@gmail.com>
2023-11-14 16:50:20 -0800

    form validation and delete fix

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -63,7 +63,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error("failed to delete invoice")
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
